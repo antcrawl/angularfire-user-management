9eba7accf350971fc96772610a1f5fcec6c5b9f2
Sharma, Ruchika <ruchika.sharma@kronos.com>
2016-03-12 17:48:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# angularfire-user-management
---
+++ fb.js
@@ -0,0 +1,25 @@
+var myApp=angular.module('app', ['firebase']);
+myApp.factory("Auth", ["$firebaseAuth",
+    function($firebaseAuth) {
+        var ref = new Firebase("https://popping-torch-5533.firebaseio.com/check1");
+        return $firebaseAuth(ref);
+    }
+]);
+
+myApp.controller('loginController', ['$scope','$firebaseObject','$firebaseAuth','Auth',loginController]);
+function loginController($scope,$firebaseObject,$firebase,Auth){
+    $scope.auth = Auth;
+    $scope.signInWithFB=function(facebook){
+
+        // any time auth status updates, add the user data to scope
+        $scope.auth.$onAuth(function(authData) {
+            $scope.authData = authData;
+            console.log("AuthData>>>",$scope.authData);
+        });
+        $scope.auth.$authWithOAuthPopup(facebook).then(function(authData) {
+            console.log("Logged in as:", authData.uid);
+        }).catch(function(error) {
+            console.error("Authentication failed: ", error);
+        });
+    };
+}
